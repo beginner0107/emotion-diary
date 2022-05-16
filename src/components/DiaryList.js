aa7495c339bb926752fd971e74b21cf7ae432adb
Beginner <dkstmdwn17@naver.com>
2022-05-16 11:28:53 +0900

    feat : 일기 날짜별 정렬, 감정 점수 별 정렬, 일기 목록 보여주는 기능

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,9 +1,95 @@
+import { useState } from "react";
+import MyButton from "./MyButton";
+import { useNavigate } from "react-router-dom";
+import DiaryItem from "./DiaryItem";
+
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select
+      className="ControlMenu"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    >
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+const sortOptionList = [
+  { value: "lastest", name: "최신순" },
+  { value: "oldest", name: "오래된 순" },
+];
+
+const filterOptionList = [
+  { value: "all", name: "전부다" },
+  { value: "good", name: "좋은 감정만" },
+  { value: "bad", name: "나쁜 감정만" },
+];
+
 const DiaryList = ({ diaryList }) => {
-  diaryList = diaryList.map((it) => {
-    return <div key={it.id}>{it.content}</div>;
+  const navigate = useNavigate();
+  const [sortType, setSortType] = useState("lastest");
+  const [filter, setFilter] = useState("all");
+
+  const getProcessedDiaryList = () => {
+    const filterCallBack = (item) => {
+      if (filter === "good") {
+        return parseInt(item.emotion) <= 3;
+      } else {
+        return parseInt(item.emotion) > 3;
+      }
+    };
+
+    const compare = (a, b) => {
+      if (sortType === "lastest") {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+
+    const filteredList =
+      filter === "all" ? copyList : copyList.filter((it) => filterCallBack(it));
+
+    const sortedList = filteredList.sort(compare);
+    return sortedList;
+  };
+
+  diaryList = getProcessedDiaryList().map((it) => {
+    return <DiaryItem key={it.id} {...it} />;
   });
 
-  return <div>{diaryList}</div>;
+  return (
+    <div className="DiaryList">
+      <div className="menu_wrapper">
+        <div className="left_col">
+          <ControlMenu
+            value={sortType}
+            onChange={setSortType}
+            optionList={sortOptionList}
+          />
+          <ControlMenu
+            value={filter}
+            onChange={setFilter}
+            optionList={filterOptionList}
+          />
+        </div>
+        <div className="right_col">
+          <MyButton
+            type={"positive"}
+            text={`새 일기쓰기`}
+            onClick={() => navigate("/new")}
+          />
+        </div>
+      </div>
+      {diaryList}
+    </div>
+  );
 };
 
 DiaryList.defaultProps = {
